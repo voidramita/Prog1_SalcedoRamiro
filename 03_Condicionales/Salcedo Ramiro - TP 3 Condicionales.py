ef902e2c10ef0d3e151bdae65db560c13b066d01
rami <ramirosalcedo55@gmail.com>
2025-10-07 16:09:18 -0300

    TP
    TP

--- 03_Condicionales/Salcedo Ramiro - TP 3 Condicionales.py
+++ 03_Condicionales/Salcedo Ramiro - TP 3 Condicionales.py
@@ -49,7 +49,7 @@ else:
 
 contraseña = input("Ingrese una contraseña: ")
 if len(contraseña) >= 8 and len(contraseña) <= 14:
-    print("Ha ingresado una contrase.")
+    print("Su contraseña ha sido ingresada correctamente.")
 else:
     print("Por favor, ingrese una contraseña entre 8 y 14 caracteres.")
 
